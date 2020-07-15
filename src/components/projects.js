04e081af9267439397250789734792f008ce045b
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-07-15 22:20:17 +0200

    create project section

---
+++ src/components/projects.js
@@ -0,0 +1,98 @@
+import { Link } from "gatsby"
+import React from "react"
+import Styled from 'styled-components'
+
+const SectionWrapper = Styled.section`
+border-top:2px solid #A9A9A9;
+border-bottom:2px solid #A9A9A9;
+padding:20px 0px; 
+display:flex;
+flex-wrap:wrap;
+align-items: center;
+justify-content: center;
+`
+
+const ProjectBox = Styled.figure`
+width:270px;
+height:200px;
+border:2px solid grey;
+border-radius:4px;
+margin:10px;
+display:flex;
+flex-direction:column;
+align-content:space-between;
+position:relative;
+.box {
+    display:none;
+    position:absolute;
+    top:0;
+    left:0;
+     color:white;
+    a{
+        color:white;
+        text-decoration:none;
+    }
+}
+ &:hover .box {
+width:100%;
+height:100%;
+    display: flex;
+    flex-direction:column;
+  align-items: center;
+  justify-content: center;
+background-color:rgba(0,0,0,0.7);
+  
+}
+  }
+
+`
+const ProjectBoxImage = Styled.img`
+height:100%;
+width:100%;
+`
+
+const ProjectBoxTitle = Styled.figcaption`
+
+`
+
+
+
+const Projects = () => (
+    <SectionWrapper>
+        <ProjectBox >
+            <div className="box">
+                <a href="">Web</a>
+                <a href="">Web</a>
+                <a href="">kod</a>
+            </div>
+            <ProjectBoxImage src="../images/project.png" />
+            <ProjectBoxTitle>
+                My Project
+            </ProjectBoxTitle>
+
+        </ProjectBox >
+        <ProjectBox >
+            <div className="box">
+                <a href="">Web</a>
+                <a href="">Web</a>
+                <a href="">kod</a>
+            </div>
+            <ProjectBoxImage src="../images/project.png" />
+            <ProjectBoxTitle>
+                My Project
+            </ProjectBoxTitle> </ProjectBox >
+        <ProjectBox >
+            <div className="box">
+                <a href="">Web</a>
+                <a href="">Web</a>
+                <a href="">kod</a>
+            </div>
+            <ProjectBoxImage src="../images/project.png" />
+            <ProjectBoxTitle>
+                My Project
+            </ProjectBoxTitle> </ProjectBox >
+    </SectionWrapper >
+)
+
+
+export default Projects
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,12 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Header from '../components/header'
-
+import Projects from '../components/projects'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Header />
+    <Projects />
   </Layout>
 )
 
