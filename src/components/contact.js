e910d2920c52dc3c678f9a8f5ed377f9ccada757
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-07-16 22:13:23 +0200

    create contact section:

---
+++ src/components/contact.js
@@ -0,0 +1,50 @@
+import React from "react"
+import Styled from 'styled-components'
+
+
+const SectionWrapper = Styled.footer`
+    width:100%;
+    height:100%;
+    display: flex;
+    flex-direction:column;
+    align-items:center;
+    padding:20px;
+   
+`
+const Icon = Styled.a`
+margin:40px;
+img{
+    height:70px;
+    :hover{
+        opacity:.5;
+    }
+}
+`
+const H2 = Styled.h2`
+letter-spacing:5px;
+font-size:60px;
+font-weight:900;
+margin:2px;
+`
+
+const Span = Styled.span`
+
+font-size:38px;
+font-weight:900;
+padding:0px 0px 50px 0px;
+`
+
+
+const Contact = () => (
+    <SectionWrapper>
+        <H2> Let`s Talk! </H2>
+        <Span> Send me a request</Span>
+        <div>
+            <Icon href="https://www.linkedin.com/in/maciej-bogda%C5%84ski/"><img src="../images/linkedin_logo.png" /></Icon>
+            <Icon href="https://github.com/mackey7"><img src="../images/github_logo.png" /></Icon>
+        </div>
+    </SectionWrapper>
+)
+
+
+export default Contact
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
 import React from "react"
-
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Header from '../components/header'
 import Projects from '../components/projects'
 import About from '../components/about'
+import Contact from '../components/contact'
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Header />
     <Projects />
     <About />
+    <Contact />
   </Layout>
 )
 
